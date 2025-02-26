6fb22e92e2eabe10f90c43fecfe3d480692ccf57
BabarAslamm <babaraslam825@gmail.com>
2025-02-26 23:49:22 +0500

    make variable reactive with ref function

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script>
+    import { ref } from 'vue';
 
     export default {
         setup () {
 
-            const message = 'Hello From Vue 3 !';
+            const message = ref('Hello From Vue 3 !');
+            const quantity = ref('1');
+
+            const increment = () => quantity.value ++;
+            const decrement = () => quantity.value --;
 
 
             return {
-                message
+                message,
+                quantity,
+                increment,
+                decrement,
+
             }
         }
 
@@ -18,6 +27,14 @@
 
 <template>
     {{ message }}
+    <br>
+    <input type="text" id="" v-model="message">
+    <hr>
+
+    <b>Quantity</b> : {{ quantity }}
+    <br>
+    <button @click="increment"> + </button>
+    <button @click="decrement"> - </button>
 </template>
 
 
